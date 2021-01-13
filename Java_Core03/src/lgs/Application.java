f1cdacca07387341bf2c84b31094a8e84166ea69
Anthony0895 <77327671+Anthony0895@users.noreply.github.com>
2021-01-13 21:56:13 +0200

    Update Application.java

--- Java_Core03/src/lgs/Application.java
+++ Java_Core03/src/lgs/Application.java
@@ -9,18 +9,18 @@ public class Application {
 		Rectangle a1 = new Rectangle ();
 		Rectangle a2 = new Rectangle (3, 1);
 		
-		System.out.println ("Прямокутник 1 = " + a1);
-		System.out.println ("Прямокутник 2 = " + a2);
+		System.out.println ("РџСЂСЏРјРѕРєСѓС‚РЅРёРє 1 = " + a1);
+		System.out.println ("ГЏГ°ГїГ¬Г®ГЄГіГІГ­ГЁГЄ 2 = " + a2);
 
 		System.out.println ();
 		
-		System.out.println ("Площа прямокутника 1 = " + a1.area (a1.getLength(),a1.getWidth()));
-		System.out.println ("Площа прямокутника 2 = " + a2.area (a2.getLength(),a2.getWidth()));
+		System.out.println ("ГЏГ«Г®Г№Г  ГЇГ°ГїГ¬Г®ГЄГіГІГ­ГЁГЄГ  1 = " + a1.area (a1.getLength(),a1.getWidth()));
+		System.out.println ("ГЏГ«Г®Г№Г  ГЇГ°ГїГ¬Г®ГЄГіГІГ­ГЁГЄГ  2 = " + a2.area (a2.getLength(),a2.getWidth()));
 		
 		System.out.println ();
 
-		System.out.println ("Периметр прямокутника 1 = " + a1.perimeter (a1.getLength(),a1.getWidth()));
-		System.out.println ("Периметр прямокутника 2 = " + a2.perimeter (a2.getLength(), a2.getWidth()));
+		System.out.println ("ГЏГҐГ°ГЁГ¬ГҐГІГ° ГЇГ°ГїГ¬Г®ГЄГіГІГ­ГЁГЄГ  1 = " + a1.perimeter (a1.getLength(),a1.getWidth()));
+		System.out.println ("ГЏГҐГ°ГЁГ¬ГҐГІГ° ГЇГ°ГїГ¬Г®ГЄГіГІГ­ГЁГЄГ  2 = " + a2.perimeter (a2.getLength(), a2.getWidth()));
 
 		System.out.println ();
 		
@@ -30,8 +30,8 @@ public class Application {
 
 		System.out.println ( a );
 		
-		System.out.println ("Площа кола = " + a.area(a.getDiameter()));
-		System.out.println ("Довжина кола = " + a.length((a.getRadius())));
+		System.out.println ("ГЏГ«Г®Г№Г  ГЄГ®Г«Г  = " + a.area(a.getDiameter()));
+		System.out.println ("Г„Г®ГўГ¦ГЁГ­Г  ГЄГ®Г«Г  = " + a.length((a.getRadius())));
 
 	}
 }
